3ebb0322d2f2a7bf6a0e05262bc4f4e5f0115e21
baibaluo <baibaluo@gmail.com>
2013-12-24 14:21:10 +0800

    add secret_token to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBlog::Application.config.secret_key_base = '4d5c2e74ba06afc79f02bb8d46329b70602fef0f5b3f33b5efdc576281b9f4f84ba47097d1e0c09b07bd300d1cfb4809b588ce8a58a6d1618bcf9e0d02391744'
